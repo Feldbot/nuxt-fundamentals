144b269dd304bfd6266f8e2b9ab60a0688240280
Daniel Feldman <feldbot@gmail.com>
2019-08-24 18:29:32 -0600

    Dynamic Routes, https://vueschool.io/lessons/nuxtjs-dynamic-routes.
    Dynamic routes tut loads a single blog post from an array of blogs. Created a posts folder, and renamed post.vue component to _id.vue. The underscore is a required Nuxt convention to show it's a URL route parameter. The route parameter name follows the underscore. So the pages/posts/_id.vue component dynamically filters through the blog array and will match URL parameter (e.g., "shimp" from localhost:3000/posts/shimp) with an id that equals shimp in the array. This data is returned in the template.

---
+++ pages/posts/_id.vue
@@ -0,0 +1,61 @@
+<template lang="html">
+  <div class="container">
+    <article>
+      <h1 class="title">{{post.title}}</h1>
+      <p>{{post.content}}</p>
+    </article>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    console.log(this.$route);
+    return {
+      // id is pages/posts/_id.vue route which
+      // dynamically resolves to URL param, e.g.,
+      // localhost:3000/posts/shimp, so 'shimp' it is!
+      // In other words, the $route obj has
+      // param.id = 'shimp' the URL parameter
+      id: this.$route.params.id,
+      posts: [
+        {
+          id: 'balut',
+          title: 'What is Balut?',
+          content: 'Balut is not salad, which is delicious.'
+        },
+        {
+          id: 'shimp',
+          title: 'Who is Shimp?',
+          content: 'Shimp is All Is One! Or NONE!'
+        }
+      ]
+    }
+  },
+  computed: {
+    post () {
+      // this.param is the data id prop above
+      return this.posts.find(post => post.id === this.id)
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+  .container {
+    display: flex;
+    justify-content: space-between;
+    line-height: 1.5;
+  }
+  article * {
+    margin-bottom: 1rem;
+  }
+  aside {
+    min-width: 280px;
+    max-width: 280px;
+    padding-left: 2rem;
+  }
+  .title {
+    font-size: 2rem;
+  }
+</style>
